930dccb44693f51aa6317420bfb5ef3a36635cf0
Russell Heilling <russell@linx.net>
2014-07-24 07:56:04 +0100

    Start the refactor of the frontend.
    Rather than focusing on entities, the new frontend will focus on actions.
    
    Discover, Acquire, Consume
    
    The new frontend will use the api where possible rather than local code.

---
+++ frontend/consume.py
@@ -0,0 +1,15 @@
+# Copyright 2013 Russell Heilling
+# pylint: disable=F0401
+from pulldb import base
+
+# pylint: disable=W0232,E1101,R0903,R0201,C0103
+
+class ConsumePage(base.BaseHandler):
+    def get(self):
+        template_values = self.base_template_values()
+        template = self.templates.get_template('consume.html')
+        self.response.write(template.render(template_values))
+
+app = base.create_app([
+    ('/consume', ConsumePage),
+])
