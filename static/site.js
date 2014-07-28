39ec3700ef020a7ef2e27cbacc80ff621ad8edc3
Russell Heilling <russell@linx.net>
2014-07-28 13:02:48 +0100

    Working oauth code.  Still needs a lot of work on the result filtering

--- static/site.js
+++ static/site.js
@@ -1,45 +1,45 @@
 var Site = {
-    clientId: '462995942151-naeu6b7vnecc6hr112a718darg0o6gqp.' + 
-	'apps.googleusercontent.com',
+    clientId: '462995942151-naeu6b7vnecc6hr112a718darg0o6gqp.' +
+        'apps.googleusercontent.com',
     apiKey: 'AIzaSyB7y3n64f5O5EQISlVQpFNvEnnr79dSVLo',
-    scopes: 'https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email',
+    scopes: 'https://www.googleapis.com/auth/plus.me ' +
+        'https://www.googleapis.com/auth/userinfo.email',
 
     init : function() {
-	if(api_ready) {
-	    this.clientAuth();
-	} else {
-	    $(document).on("pulldbAuthReady", $.proxy(this.clientAuth, this));
-	}
-	this.authEvents();
+        if(api_ready) {
+            this.clientAuth();
+        } else {
+            $(document).on("pulldbAuthReady", $.proxy(this.clientAuth, this));
+        }
+        this.authEvents();
         this.subscriptionEvents();
     },
 
     clientAuth: function() {
-	console.log("doing clientauth...", this);
-	gapi.client.setApiKey(this.apiKey);
-	gapi.auth.authorize({
-	    client_id: this.clientId, scope: this.scopes, immediate: true
-	}, this.handleAuthResult);
+        console.log("doing clientauth...", this);
+        gapi.client.setApiKey(this.apiKey);
+        gapi.auth.authorize({
+            client_id: this.clientId, scope: this.scopes, immediate: true
+        }, this.handleAuthResult);
     },
 
     handleAuthResult: function (authResult) {
-	var auth_panel = $('#auth-required');
-	console.log(authResult);
-	if (authResult && !authResult.error) {
-	    auth_panel.hide();
-	    $.event.trigger(
-		{"type": "pulldbAuthorised"}, [authResult.access_token]);
-	} else {
-	    auth_panel.show();
-	}
+        var auth_panel = $('#auth-required');
+        if (authResult && !authResult.error) {
+            auth_panel.hide();
+            $.event.trigger(
+                {"type": "pulldbAuthorised"}, [authResult.access_token]);
+        } else {
+            auth_panel.show();
+        }
     },
 
     authEvents : function() {
-	$("#auth-required .btn").on("click", $.proxy(function() {
-	    gapi.auth.authorize({
-		client_id: this.clientId, scope: this.scopes, immediate: false,
-	    }, this.handleAuthResult);
-	}, this));
+        $("#auth-required .btn").on("click", $.proxy(function() {
+            gapi.auth.authorize({
+                client_id: this.clientId, scope: this.scopes, immediate: false,
+            }, this.handleAuthResult);
+        }, this));
     },
 
     subscriptionEvents : function () {
@@ -72,6 +72,14 @@ var Site = {
         });
     },
 };
+
+// Triggers for oauth token availability
+var api_ready;
+function handleApiLoad() {
+  console.log("trigger authready...");
+  $.event.trigger({"type": "pulldbAuthReady"});
+  api_ready = true;
+}
 
 $(document).ready( function() {
     Site.init();
