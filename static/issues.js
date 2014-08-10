a28a53728df4c934e8c3c27936260d991d449d9c
Russell Heilling <russell@linx.net>
2014-08-10 13:15:19 +0100

    Implement issue pull toggle

--- static/issues.js
+++ static/issues.js
@@ -5,6 +5,46 @@ var Issues = {
         this.templateLoad();
     },
 
+    togglePulled: function() {
+        var pull = $(this);
+        function togglePulled() {
+            pull.find("i.fa").toggleClass(
+                "fa-square fa-square-o");
+            if(pull.data("pulled") == "True") {
+                pull.data("pulled", "False");
+            } else {
+                pull.data("pulled", "True");
+            }
+        }
+        if(pull.data("pulled") == "True") {
+            console.log('Removing pull...');
+            Site.apiFetch({
+                method: "POST",
+                url: "/api/pulls/update",
+                data: JSON.stringify({
+                    "unpull": [String(pull.data("pull"))],
+                }),
+                success: togglePulled,
+            });
+        } else {
+            console.log('Pulling...', JSON.stringify({
+                pull: [pull.data("pull")],
+            }));
+            Site.apiFetch({
+                method: "POST",
+                url: "/api/pulls/update",
+                data: JSON.stringify({
+                    "pull": [String(pull.data("pull"))],
+                }),
+                success: togglePulled,
+            });
+        }
+    },
+
+    toggleEvents : function () {
+        $("a.pull").on("click", Issues.togglePulled);
+    },
+
     templateLoad: function() {
         $.get('/static/issue_panel.jst', function(data) {
             issue_panel = jsontemplate.Template(data);
@@ -33,4 +73,5 @@ function renderNewPulls(data) {
         $("div#content").append($(panel_content));
     }
     $("div#content [data-toggle=tooltip]").tooltip();
+    Issues.toggleEvents();
 }
